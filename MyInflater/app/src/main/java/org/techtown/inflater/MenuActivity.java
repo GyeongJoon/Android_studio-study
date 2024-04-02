2462439af5ea1ab633109716b00ee48ee8bf85c6
GyeongJoon <jjangjoon328@naver.com>
2024-04-02 19:54:47 +0900

    inflater

--- MyInflater/app/src/main/java/org/techtown/inflater/MenuActivity.java
+++ MyInflater/app/src/main/java/org/techtown/inflater/MenuActivity.java
@@ -26,7 +26,7 @@ public class MenuActivity extends AppCompatActivity {
         container = findViewById(R.id.container); // XML 레이아웃에서 container라는 ID를 가진 뷰를 찾아서 변수에 할당합니다.
 
         Button button2 = findViewById(R.id.button2); // XML 레이아웃에서 button2라는 ID를 가진 버튼을 찾아서 변수에 할당합니다.
-        button2.setOnClickListener(new View.OnClickListener() { // 버튼에 클릭 리스너를 등록합니다.
+        button2.setOnClickListener(new View.OnClickListener() { // 버튼에 클릭 리스너를 등록합니다
             @Override
             public void onClick(View v) { // 버튼이 클릭되었을 때 실행되는 메서드입니다.
                 addLayout(); // addLayout 메서드를 호출하여 부분 화면을 추가합니다.
