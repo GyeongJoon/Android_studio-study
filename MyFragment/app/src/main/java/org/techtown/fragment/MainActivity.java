97d62bdf529ced95f13355e673afcbead087a599
GyeongJoon <jjangjoon328@naver.com>
2024-04-04 22:31:29 +0900

    fragment

--- MyFragment/app/src/main/java/org/techtown/fragment/MainActivity.java
+++ MyFragment/app/src/main/java/org/techtown/fragment/MainActivity.java
@@ -9,11 +9,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    MainFragment mainFragment;
+    MenuFragment menuFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.mainFragment);
+        mainFragment = new MainFragment();
+    }
+
+    public void onFragmentChanged (int index) {
+        if (index == 0) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, mainFragment).commit();
+        } else if (index == 1) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, menuFragment).commit();
+        }
     }
 }
