e9f12a7097ba1731ead0079044c0803adf6610f5
GyeongJoon <jjangjoon328@naver.com>
2024-04-07 12:00:11 +0900

    fragment

--- MyFragment2/app/src/main/java/org/techtown/fragment2/ListFragment.java
+++ MyFragment2/app/src/main/java/org/techtown/fragment2/ListFragment.java
@@ -16,37 +16,43 @@ public class ListFragment extends Fragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        // Fragment가 Activity에 Attach될 때 호출되는 메서드
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Fragment의 뷰를 생성하고 반환하는 메서드
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_list, container, false);
 
+        // 첫 번째 버튼 설정
         Button button = rootView.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // 첫 번째 버튼 클릭 시 동작할 내용
             }
         });
 
+        // 두 번째 버튼 설정
         Button button2 = rootView.findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // 두 번째 버튼 클릭 시 동작할 내용
             }
         });
 
+        // 세 번째 버튼 설정
         Button button3 = rootView.findViewById(R.id.button3);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // 세 번째 버튼 클릭 시 동작할 내용
             }
         });
 
         return rootView;
     }
 }
+
