4a80f9007d3a906d45317101267acebb90b43c82
GyeongJoon <jjangjoon328@naver.com>
2024-04-11 16:46:13 +0900

    M7

---
+++ Mission/M7/app/src/main/java/org/techtown/m7/MenuActivity.java
@@ -0,0 +1,34 @@
+package org.techtown.m7;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                Toast toastView = Toast.makeText(getApplicationContext(), "로그인 화면입니다.", Toast.LENGTH_LONG);
+
+                toastView.show();
+            }
+        });
+    }
+}
